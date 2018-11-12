91ae02f2a3970de23ecea7befcf372473ab6471f
Michael Salvati <print@3daddict.com>
2018-11-11 22:20:02 -0800

    sign up form and route created

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// GET /register
+router.get('/register', function(req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /register
+router.post('/register', function(req, res, next) {
+  // return res.render('register', { title: 'register' });
+  return res.send('User Created!');
+});
+
 // GET /
 router.get('/', function(req, res, next) {
   return res.render('index', { title: 'Home' });
